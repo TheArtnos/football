fac9b9811af4ae62905b449e87ad72680cb8e818
TheArtnos <theartnos@gmail.com>
2025-10-27 12:36:22 +0330

    Add files via upload

--- api/matches.js
+++ api/matches.js
@@ -1,7 +1,12 @@
-// /api/matches.js
 export default async function handler(req, res) {
+  const { dateFrom, dateTo } = req.query;
+
+  if (!dateFrom || !dateTo) {
+    return res.status(400).json({ error: "Missing dateFrom or dateTo" });
+  }
+
   const response = await fetch(
-    `https://cors-anywhere.herokuapp.com/https://api.football-data.org/v4/matches?dateFrom=${dateFromStr}&dateTo=${dateToStr}`,
+    `https://cors-anywhere.herokuapp.com/https://api.football-data.org/v4/matches?dateFrom=${dateFrom}&dateTo=${dateTo}`,
     {
       headers: {
         "X-Auth-Token": "8f98efd8340543d693a925ecd26673b4",
